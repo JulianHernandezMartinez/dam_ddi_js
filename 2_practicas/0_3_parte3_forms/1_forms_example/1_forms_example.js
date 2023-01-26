1f09aa555e6eac3c92dfb814aab0c67365408f3b
JulianHernandezMartinez <profefp2223@gmail.com>
2023-01-26 19:28:37 +0100

    Mejoras -reedicion

--- 2_practicas/0_3_parte3_forms/1_forms_example/1_forms_example.js
+++ 2_practicas/0_3_parte3_forms/1_forms_example/1_forms_example.js
@@ -4,16 +4,14 @@ const nombreOut = document.querySelector('#nombre_out')
 
 const myform = document.querySelector('#myform');
 
-
 mydiv.textContent = 'How are you feeling today?';
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log("clicked!");
-    const name = String(nameInp.value);
-    saluda(name);
+    const name = nameInp.value;
+    nombreOut.textContent=name;
 
     div1.style.display = 'none';
     div2.style.display = 'block';
-    interval = setInterval(countDown, 1000);
 });
