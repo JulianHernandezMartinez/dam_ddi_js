9457f60092a84561aa418572d0c50a928654c1e0
JulianHernandezMartinez <profefp2223@gmail.com>
2024-01-25 20:27:19 +0100

    fetch mejorado con Object y clases

--- 1_playground/Set_7_Fetch_http/Set_6_3_fetch_JSON_server/set_6_fetch_remote.js
+++ 1_playground/Set_7_Fetch_http/Set_6_3_fetch_JSON_server/set_6_fetch_remote.js
@@ -6,7 +6,7 @@
 // $ sudo npm install -g json-server
 
 // start JSON Server 
-// $ cd dam_ddi_js/1_playground/Set_6_Fetch_http/Set_6_3_fetch_JSON_server/JsonDDBB
+// $ cd /home/vboxuser/dam_ddi_js/1_playground/Set_6_Fetch_http/Set_6_3_fetch_JSON_server/JsonDDBB
 // $ json-server --watch db.json
 // $ json-server --watch localidades.json
 /* 
@@ -22,6 +22,12 @@ Home
 http://localhost:3000
 
 */
+
+// JSON in MDN   --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON 
+// JSON standard --> https://datatracker.ietf.org/doc/html/rfc8259
+// JSON grammar  --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON#full_json_grammar
+
+
 fetch(`http://localhost:3000/progbooks`)
 .then((response) => response.json())
 .then((data) => {
@@ -34,8 +40,20 @@ fetch(`http://localhost:3000/localidades?provincia=Granada`)
 .then((response) => response.json())
 .then((data) => {
   allitems = data;
+  console.log('fetch localidades con filtro Provincia = Granada');
   console.log (allitems);
 });
+
+class Localidad {
+  localidad;
+  provincia;
+  constructor(localidad,provincia) {
+    this.localidad = localidad;
+    this.provincia = provincia;
+  }
+}
+
+const nuevalocalidad = new Localidad("Benaojan","Malaga");
 
 const newlocality = {
   "localidad" : "Lugros",
@@ -44,10 +62,11 @@ const newlocality = {
 
 console.log(JSON.stringify(newlocality));
 
+console.log(JSON.stringify(nuevalocalidad));
 
 fetch(`http://localhost:3000/localidades`,{
   method: 'POST', 
-  body: JSON.stringify(newlocality),
+  body: JSON.stringify(nuevalocalidad),
   headers: {
     "Content-type": "application/json; charset=UTF-8"
     }
@@ -55,5 +74,6 @@ fetch(`http://localhost:3000/localidades`,{
 .then((response) => response.json())
 .then((data) => {
   postresult = data;
+  console.log('resultado del post de Nueva Localidad')
   console.log (postresult);
 });
